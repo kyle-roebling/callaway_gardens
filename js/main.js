2d33ce59dcd0b976282ec14c1297ce07ec320cf4
kyle <roebling@wisc.edu>
2021-03-20 12:26:05 -0500

    1). Added basemap 2). Added Explore Button

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+//Set up map variable
+var mymap = L.map('mapid').setView([32.83736, -84.85368], 13);
+
+//Add basemap to application
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/satellite-v9',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoia3JvZWJsaW5nIiwiYSI6ImNqeXczaGplMjB3YjgzYmxyZGU1OG90bXUifQ.ItIrq8YGHvZIilkcx-U8Ag'
+}).addTo(mymap);
